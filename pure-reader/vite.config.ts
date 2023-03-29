8da7e263f427d8385b28cb9cd5285d0747fb5478
marvel <outlinmt@outlook.com>
2023-03-30 00:24:27 +0800

    vite配置

--- pure-reader/vite.config.ts
+++ pure-reader/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import { internalIpV4 } from 'internal-ip'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(async () => {
+  const host = await internalIpV4()
+  const config = {
+    plugins: [vue()],
+    server: {
+      host: '0.0.0.0', // listen on all addresses
+      port: 8080,
+      strictPort: true,
+      hmr: {
+        protocol: 'ws',
+        host,
+        port: 5183,
+      },
+    },
+  }
+  return config
 })
