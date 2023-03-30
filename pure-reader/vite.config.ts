310ee96971109f693dba00860b48106fa73d7b17
Marvel YAO <outlinmt@outlook.com>
2023-03-30 16:34:29 +0800

    配置

--- pure-reader/vite.config.ts
+++ pure-reader/vite.config.ts
@@ -16,6 +16,15 @@ export default defineConfig(async () => {
         host,
         port: 5183,
       },
+      envPrefix: ['VITE_', 'TAURI_'],
+      build: {
+        // Tauri supports es2021
+        target: ['es2021', 'chrome100', 'safari13'],
+        // don't minify for debug builds
+        minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_DEBUG,
+      },
     },
   }
   return config
