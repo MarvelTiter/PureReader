9a0096cbd918142b54faa15f3785d542e72e1d8b
marvel <outlinmt@outlook.com>
2023-04-02 23:59:04 +0800

    maui shell app

---
+++ MauiPureReader/PureReader/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PureReader
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
